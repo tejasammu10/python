b55496ee431c54f07944f5faf37931e54fa9158a
tejasammu10 <tejasammu22@gmail.com>
2026-06-06 20:50:11 +0530

    7th commit

---
+++ amount.py
@@ -0,0 +1,24 @@
+x=int(input())
+
+note_1=x//100#100 rupee notes
+
+note_2=x%100
+
+note_3=note_2//50#50 rupee notes
+
+note_4=note_2%50
+
+note_5=note_4//10#10 rupee notes
+
+print(note_1) 
+print(note_3)
+print(note_5)
+
+
+
+
+
+
+
+
+
---
+++ tree.py
@@ -0,0 +1,14 @@
+
+tree1 = 10
+tree2 = 15
+tree3 = 20
+tree4 = 25
+tree5 = 30
+
+Sum = tree1+tree2+tree3+tree4+tree5
+
+print(Sum)
+
+Average=(Sum)/5 
+
+print(Average)

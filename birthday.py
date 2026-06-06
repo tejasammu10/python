a1853c2c5d1a5ea1f5fdfb7de671200c5eea3102
tejasammu10 <tejasammu22@gmail.com>
2026-06-06 20:54:29 +0530

    8th commit

---
+++ birthday.py
@@ -0,0 +1,20 @@
+friend1 = "Amit"
+friend1_birthday = "August 12"
+
+friend2 = "Sneha"
+friend2_birthday = "October 5"
+
+friend3 = "Rahul"
+friend3_birthday = "January 22"
+
+friend4 = "Priya"
+friend4_birthday = "March 14"
+
+friend5 = "Vikram"
+friend5_birthday = "December 30"
+
+print(friend1 + "'s birthday is on " + friend1_birthday)
+print(friend2 + "'s birthday is on " + friend2_birthday)
+print(friend3 + "'s birthday is on " + friend3_birthday)
+print(friend4 + "'s birthday is on " + friend4_birthday)
+print(friend5 + "'s birthday is on " + friend5_birthday)

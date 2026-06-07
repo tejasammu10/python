93fc365606d253ccef4056066944c08021c7a481
tejasammu10 <tejasammu22@gmail.com>
2026-06-07 19:16:50 +0530

    9th commit

---
+++ my_daily_routine.py
@@ -0,0 +1,23 @@
+print("My Daily Routine")
+
+print("I wake up at 7 AM.")
+
+print("I go to school at 8 AM.")
+
+print("I play with my friends in the evening.")
+
+print("I study for 2 hours at night.")
+
+print("Total study hours in a week:", 2 * 7)
+
+print("My Daily Routine")
+
+print("I wake up at 7 AM.")
+
+print("I go to school at 8 AM.")
+
+print("I play with my friends in the evening.")
+
+print("I study for 2 hours at night.")
+
+print("Total study hours in a week:", 2 * 7)

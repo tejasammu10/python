c49c07fab2d13535d8741a0cb37a01c6c2f23284
tejasammu10 <tejasammu22@gmail.com>
2026-06-07 19:27:24 +0530

    10th commit

---
+++ congratulations.py
@@ -0,0 +1,7 @@
+
+uppercase_message = "CONGRATULATIONS"
+
+lowercase_message = uppercase_message.lower()
+
+print("Original Message:", uppercase_message)
+print("Converted Message:", lowercase_message)
---
+++ square_root.py
@@ -0,0 +1,3 @@
+int(input())
+y=(input())**1/2
+print(y)

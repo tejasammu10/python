36fc02e231a0f2adaa47ec9f65731d7bb0d3e823
tejasammu10 <tejasammu22@gmail.com>
2026-06-12 22:33:05 +0530

    11th commit

---
+++ summertimes.py
@@ -0,0 +1,15 @@
+print("what is the temperature today?")
+x=float(input())
+if x>=0:print("It is burning stay home")
+if x>=35:print("Its a hot day wear light clothes and enjoy")
+else:print("It is a cold day please wear heavy cloths for the weather")
+
+
+
+
+
+
+
+
+
+

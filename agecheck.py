ba09119cb2b05ea705a37df939910abd89dc0a67
tejasammu10 <tejasammu22@gmail.com>
2026-06-23 21:28:21 +0530

    18th commit

---
+++ agecheck.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age: "))
+
+if age >= 10:
+    if age <= 20:
+        print("The age is between 10 and 20.")
+    else:
+        print("The age is greater than 20.")
+else:
+    print("The age is less than 10.")

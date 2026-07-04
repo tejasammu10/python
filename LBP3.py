cd7825982df79277f2bfe1d2e344b25d8a3ea8f1
tejasammu10 <tejasammu22@gmail.com>
2026-07-04 20:36:54 +0530

    19th commit

---
+++ LBP3.py
@@ -0,0 +1,10 @@
+n=int(input()) 
+print("numbers from n to 1 are")
+
+for i in range(n,0,-1):
+    print(i)
+
+
+
+
+
---
+++ Loopsboilerplate2.py
@@ -0,0 +1,8 @@
+
+string=str(input())
+string2=""
+
+for i in string:
+    string2= i + string2
+    print(string2)
+

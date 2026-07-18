c7073264ff0120937acaa5875c93ea80698b93e4
tejasammu10 <tejasammu22@gmail.com>
2026-07-18 20:51:13 +0530

    test

---
+++ test.py
@@ -0,0 +1,15 @@
+secret=20
+guess=int(input())
+
+if guess<=20 and guess>=0:print("hot")
+elif guess>20 and guess<=40:print("hot")
+elif guess>40 and guess<=60:print("cold")
+else:print("Ice cold")
+
+if guess==20:print("You won")
+
+
+ 
+    
+
+    
